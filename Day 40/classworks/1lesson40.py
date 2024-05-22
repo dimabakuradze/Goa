e971e6cf1f9d578dff04c72d0ea256c8287f7a0b
dimabakuradze <xatunan12@gmail.com>
2024-05-22 15:10:33 +0400

    Goa-academy

---
+++ Day 40/classworks/1lesson40.py
@@ -0,0 +1,32 @@
+# N1
+
+# Write a function that takes an array of numbers
+# and returns the sum of the numbers. 
+# The numbers can be negative or non-integer. 
+# If the array does not contain any numbers then you should return 0.
+
+# def sum_array(a):
+#     sum = 0
+    
+#     for num in a:
+#         sum = sum + num
+        
+#     return sum
+
+
+
+
+
+# N2
+
+# Write a function which calculates 
+# the average of the numbers in a given list.
+# Note: Empty arrays should return 0.
+
+# def find_average(numbers):
+#     if not numbers:
+#         return 0
+#     return sum(numbers) / len(numbers)
+
+
+
