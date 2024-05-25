5559e8d2a220259c7f1e99e36e93667eeb6bbd17
dimabakuradze <xatunan12@gmail.com>
2024-05-25 23:08:53 +0400

    Goa-academy

--- Day 09/classworks/1lesson9.py
+++ Day 09/classworks/1lesson9.py
@@ -1,3 +1,16 @@
-name = int(input('enter number'))
+# name = int(input('enter number'))
 
-print(name > 1 and name < 5 ) 
+# print(name > 1 and name < 5 ) 
+
+
+# შემოვიტანოთ სტრინგი და გავმრავლოთ ხუთზე
+string_input = input("შეიყვანეთ სტრინგი: ")
+multiplied_string = string_input * 5
+
+# შემოვიტანოთ რიცხვი და გავმრავლოთ ხუთზე
+number_input = int(input("შეიყვანეთ რიცხვი: "))
+multiplied_number = number_input * 5
+
+# დავბეჭდოთ ორივე შედეგი
+print("გამრავლებული სტრინგი: ", multiplied_string)
+print("გამრავლებული რიცხვი: ", multiplied_number)
